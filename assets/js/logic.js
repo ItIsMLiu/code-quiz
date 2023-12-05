2757805370594f0ea4c7c54e4d8195de23c7dfc8
ItIsMLiu <mliu.design@outlook.com>
2023-12-05 18:47:15 +0000

    logic and questions js files developed.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,48 +7,152 @@ let questionTitleEl = document.getElementById("question-title");
 let choicesEl = document.getElementById("choices");
 let endScreenEl = document.getElementById("end-screen");
 
+// Create unordered list element
+let listEl = document.createElement("ul");
+// Create unordered list item elements
+let li1 = document.createElement("li");
+let li2 = document.createElement("li");
+let li3 = document.createElement("li");
+let li4 = document.createElement("li");
+// Create button element for each list item element
+let li1Btn = document.createElement("button");
+let li2Btn = document.createElement("button");
+let li3Btn = document.createElement("button");
+let li4Btn = document.createElement("button");
+// Append child ul to choices <div>
+choicesEl.appendChild(listEl);
+// Append child <li> to <ul>
+listEl.appendChild(li1);
+listEl.appendChild(li2);
+listEl.appendChild(li3);
+listEl.appendChild(li4);
+// Append child <button> to <li>
+li1.appendChild(li1Btn);
+li2.appendChild(li2Btn);
+li3.appendChild(li3Btn);
+li4.appendChild(li4Btn);
 
-// Select time span element by ID
-let timerEl = document.querySelector("#time");
-let secondsLeft = 76;
+// change multiple li elements styles
+let liEl = document.querySelectorAll("li");
+for (let i=0; i<liEl.length; i++) {
+    liEl[i].setAttribute("style", "list-style: none; background-color: transparent;");
+};
 
-function setTimer() {
- let timeInterval = setInterval(function() {
-    secondsLeft--;
-    timerEl.textContent = secondsLeft;
-    if(secondsLeft === 0) {
-        clearInterval(timeInterval);
-        questionsEl.style.display = 'none';
-        endScreenEl.style.display = 'block';
+function countdown() {
+    // Select time span element by ID
+    let timerEl = document.querySelector("#time");
+    let secondsLeft = 75;
+    
+    let timeInterval = setInterval(function() {
+        if (secondsLeft > 0) {
+            // Set the `textContent` of `timerEl` to show the time left
+            timerEl.textContent = secondsLeft;
+            // Decrement `secondsLeft` by 1
+            secondsLeft--;
+        } else if (secondsLeft === 0) {
+            timerEl.textContent = secondsLeft;
+            questionsEl.style.display = 'none';
+            endScreenEl.style.display = 'block';
+        }
+    }, 1000);   
+}
+
+function newQuestion() {
+    for (var i=0; i<questionsArray.length; i++) {
+        li1Btn.textContent = questionsArray[i].answerOptions[0];
+        li2Btn.textContent = questionsArray[i].answerOptions[1];
+        li3Btn.textContent = questionsArray[i].answerOptions[2];
+        li4Btn.textContent = questionsArray[i].answerOptions[3];
+        questionTitleEl.textContent = questionsArray[i].ask;
     }
- }, 1000);   
+
 }
 
 // Listen for a click event on start quiz button
 startQuizBtn.addEventListener("click", function() {
-    setTimer();
+    //start timer
+    countdown();
+    
+    //hide start-screen <div> and unhide questions <div>
     startScreenEl.style.display = 'none';
     questionsEl.style.display = 'block';
 
-    // Create ordered list element
+    newQuestion(questionsArray);
+})
+
+li1Btn.addEventListener("click", function() {
+    listEl.textContent = " ";
+    questionTitleEl.textContent = " ";
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+// Access start quiz button HTML element
+let startQuizBtn = document.getElementById("start");
+
+let startScreenEl = document.getElementById("start-screen");
+let questionsEl = document.getElementById("questions");
+let questionTitleEl = document.getElementById("question-title");
+let choicesEl = document.getElementById("choices");
+let endScreenEl = document.getElementById("end-screen");
+
+function countdown() {
+    // Select time span element by ID
+    let timerEl = document.querySelector("#time");
+    let secondsLeft = 75;
+    
+    let timeInterval = setInterval(function() {
+        if (secondsLeft >= 1) {
+            // Set the `textContent` of `timerEl` to show the time left
+            timerEl.textContent = secondsLeft;
+            // Decrement `secondsLeft` by 1
+            secondsLeft--;
+        } else if (secondsLeft === 0) {
+            questionsEl.style.display = 'none';
+            endScreenEl.style.display = 'block';
+        }
+    }, 1000);   
+}
+
+// Listen for a click event on start quiz button
+startQuizBtn.addEventListener("click", function() {
+    //start timer
+    countdown();
+    //hide start-screen <div> and unhide questions <div>
+    startScreenEl.style.display = 'none';
+    questionsEl.style.display = 'block';
+
+    // Create unordered list element
     let listEl = document.createElement("ul");
-    // Create ordered list items
+    // Create unordered list item elements
     let li1 = document.createElement("li");
     let li2 = document.createElement("li");
     let li3 = document.createElement("li");
     let li4 = document.createElement("li");
-
+    // Create button element for each list item element
     let li1Btn = document.createElement("button");
     let li2Btn = document.createElement("button");
     let li3Btn = document.createElement("button");
     let li4Btn = document.createElement("button");
-
+    // Append child ul to choices <div>
     choicesEl.appendChild(listEl);
+    // Append child <li> to <ul>
     listEl.appendChild(li1);
     listEl.appendChild(li2);
     listEl.appendChild(li3);
     listEl.appendChild(li4);
-
+    // Append child <button> to <li>
     li1.appendChild(li1Btn);
     li2.appendChild(li2Btn);
     li3.appendChild(li3Btn);
@@ -65,6 +169,10 @@ startQuizBtn.addEventListener("click", function() {
     li3Btn.textContent = questionsArray[0].answerOptions[2];
     li4Btn.textContent = questionsArray[0].answerOptions[3];
     questionTitleEl.textContent = questionsArray[0].ask;
-
-    
 })
+
+li1Btn.addEventListener("click", function() {
+    listEl.textContent = " ";
+    questionTitleEl.textContent = " ";
+})
+*/
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,49 +1,27 @@
 let questionsArray = [
     {
-        ask: 'Commonly used data types DO NOT include:',
+        ask: '1/5. Commonly used data types DO NOT include:',
         answerOptions: ['A. strings', 'B. booleans', 'C. alerts', 'D. numbers'],
         correctAnswer: 'C' 
     },
     {
-        ask: 'The condition in an if / else statment is enclosed within _____.',
+        ask: '2/5. The condition in an if / else statment is enclosed within _____.',
         answerOptions: ['A. quotes', 'B. curlybrackets', 'C. parentheses', 'D. square brackets'],
         correctAnswer: 'C' 
     },
     {
-        ask: 'Arrays in JavaScript can be used to store _____.',
+        ask: '3/5. Arrays in JavaScript can be used to store _____.',
         answerOptions: ['A. numbers and strings', 'B. other arrays', 'C. booleans', 'D. all of the above'],
         correctAnswer: 'D' 
     },
     {
-        ask: 'String values must be enclosed within _____ when being assigned to variables',
+        ask: '4/5. String values must be enclosed within _____ when being assigned to variables',
         answerOptions: ['A. commas', 'B. curly brackets', 'C. quotes', 'D. parentheses'],
         correctAnswer: 'C' 
     },
     {
-        ask: 'A very useful tool used during development and debugging for printing content to the debugger Is:',
+        ask: '5/5. A very useful tool used during development and debugging for printing content to the debugger Is:',
         answerOptions: ['A. JavaScript', 'B. terminal/bash', 'C. for loops', 'D. console.log'],
         correctAnswer: 'D' 
     },
-/*
-    {
-        ask: '',
-        answerOptions: ['A. ', 'B. ', 'C. ', 'D. '],
-        correctAnswer: '' 
-    },
-    {
-        ask: '',
-        answerOptions: ['A. ', 'B. ', 'C. ', 'D. '],
-        correctAnswer: '' 
-    },
-    {
-        ask: '',
-        answerOptions: ['A. ', 'B. ', 'C. ', 'D. '],
-        correctAnswer: '' 
-    },
-    {
-        ask: '',
-        answerOptions: ['A. ', 'B. ', 'C. ', 'D. '],
-        correctAnswer: '' 
-    },
-    */
 ];
